0f75ce552c2dc7ac0d0b7d166230d1c4ee1ce894
Dmitry Shagurov <shagurovda@gmail.com>
2022-08-21 11:29:11 +0400

    included express & dotenv; basic listeting script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hewwo!");
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 5000;
+
+const app = express();
+app.use(express.json());
+
+const startListening = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Listening PORT ${PORT}`))
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startListening();
